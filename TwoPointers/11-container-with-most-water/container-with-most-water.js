9713adbae71adc6e406b67ad7e0f555201272709
Δbhīņay Řaj <31544581+abrajByte@users.noreply.github.com>
2025-06-09 22:48:32 +0530

    Time: 1 ms (95.36%) | Memory: 63.7 MB (11.09%) - LeetSync

---
+++ TwoPointers/11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let i=0;
+    let j= height.length-1
+    let maxArea = 0
+    
+    while(i<j){
+        const area = (j-i) * Math.min(height[i],height[j])
+        maxArea = Math.max(maxArea, area)
+        if(height[i]<height[j]){
+            i++
+        }else{
+            j--
+        }
+    }
+    return maxArea
+};
