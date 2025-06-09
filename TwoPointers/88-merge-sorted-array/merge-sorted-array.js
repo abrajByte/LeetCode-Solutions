aa3f36ecf9b7be472a793a724ab0416b0642c609
Δbhīņay Řaj <31544581+abrajByte@users.noreply.github.com>
2025-06-09 22:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (10.36%) - LeetSync

---
+++ TwoPointers/88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let i = m - 1; // at nums1 valid index (till nums1 last element)
+    let j = n - 1; // at nums2 last index
+    let k = m + n - 1; // at nums1 last index
+
+    while (j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+};
