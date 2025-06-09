23d8fea18ef12b50f4b9a17ead8ba4a7d335ea8d
Δbhīņay Řaj <31544581+abrajByte@users.noreply.github.com>
2025-06-09 22:55:03 +0530

    Time: 5 ms (81.39%) | Memory: 57.7 MB (71.99%) - LeetSync

---
+++ SlidingWindow-FixedSize/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const set = new Set()
+    let l = 0
+    let longest = 0
+
+    for(let r=0;r<s.length;r++){
+        while(set.has(s[r])){
+            set.delete(s[l])
+            l++
+        }
+
+        longest = Math.max(longest, (r-l)+1)
+        set.add(s[r])
+    }
+    return longest
+};
